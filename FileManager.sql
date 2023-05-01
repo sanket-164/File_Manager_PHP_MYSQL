dab7374a902591bdb01cfff195484255f382a726
sanket-164 <sanketsadadiya53@gmail.com>
2023-05-01 15:15:59 +0530

    Created Database and folder structure for project

---
+++ FileManager.sql
@@ -0,0 +1,25 @@
+USE mysql;
+
+CREATE DATABASE file_manager;
+
+USE file_manager;
+
+CREATE TABLE user_info(
+    user_id INTEGER PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    username VARCHAR(100) NOT NULL UNIQUE KEY,
+    firstname VARCHAR(100) NOT NULL,
+    middlename VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+    user_image BLOB NOT NULL,
+    user_email VARCHAR(100) NOT NULL UNIQUE KEY,
+    user_mobile VARCHAR(100) NOT NULL UNIQUE KEY,
+    user_dob date NOT NULL,
+);
+
+-- CREATE TABLE sanket(
+--     file_id INTEGER PRIMARY KEY,
+--     file_name VARCHAR(255) NOT NULL,
+--     file_content BLOB NOT NULL,
+--     upload_time datetime NOT NULL,
+-- );
